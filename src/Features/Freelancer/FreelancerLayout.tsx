71a33214b2e8b87bfc0663025240dca274dcacba
AmirReza Jamali <amir7jamali.ar@yahoo.com>
2024-03-15 00:08:13 +0330

    Vercel errors fixed

--- src/Features/Freelancer/FreelancerLayout.tsx
+++ src/Features/Freelancer/FreelancerLayout.tsx
@@ -1,3 +1,4 @@
+import { FC } from "react";
 import { NavLink } from "react-router-dom";
 import { FreelancerPanelSidebar } from "../../Constants/NavLinks";
 import AppLayout from "../Shared/UI/Layouts/AppLayout";
@@ -7,21 +8,30 @@ const FreelancerDashboardLayout = () => {
   return (
     <AppLayout>
       <Sidebar>
-        {FreelancerPanelSidebar.map(({ Icon, link, title }) => (
-          <li key={title}>
-            <NavLink
-              to={link}
-              className={({ isActive }) =>
-                isActive
-                  ? "bg-primary-blue-100/50 text-primary-blue-300 flex items-center gap-x-2 transition px-2 py-1.5 rounded-lg"
-                  : "flex items-center gap-x-2 transition px-2 py-1.5 rounded-lg text-primary-gray-600 hover:bg-primary-blue-100/50 hover:text-primary-blue-600"
-              }
-            >
-              <Icon />
-              <span>{title}</span>
-            </NavLink>
-          </li>
-        ))}
+        {FreelancerPanelSidebar.map(
+          ({
+            Icon,
+            link,
+            title,
+          }: {
+            Icon: FC;
+            link: string;
+            title: string;
+          }) => (
+            <li key={title}>
+              <NavLink
+                to={link}
+                className={({ isActive }) =>
+                  isActive
+                    ? "bg-primary-blue-100/50 text-primary-blue-300 flex items-center gap-x-2 transition px-2 py-1.5 rounded-lg"
+                    : "flex items-center gap-x-2 transition px-2 py-1.5 rounded-lg text-primary-gray-600 hover:bg-primary-blue-100/50 hover:text-primary-blue-600"
+                }>
+                <Icon />
+                <span>{title}</span>
+              </NavLink>
+            </li>
+          )
+        )}
       </Sidebar>
     </AppLayout>
   );
