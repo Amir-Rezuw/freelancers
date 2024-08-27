85e653b8582a4f213c5905a2f56b7e0c819e23f5
AmirReza Jamali <amir7jamali.ar@yahoo.com>
2024-08-27 13:07:23 +0330

    fixed access denied page

--- src/Pages/AccessDenied.tsx
+++ src/Pages/AccessDenied.tsx
@@ -1,9 +1,34 @@
 import { MdLogin } from "react-icons/md";
 import { MessagesText } from "../Constants/Enums/Messages";
 import useLogout from "../Features/Authentication/Hooks/useLogout";
+import { Link, useNavigate } from "react-router-dom";
+import useGetUserProfile from "../Features/Authentication/Hooks/useGetUserProfile";
+import { useAuthCtx } from "../Features/Authentication/Context/Auth.ctx";
+import Loading from "../Features/Shared/UI/Loading";
+import ErrorLabel from "../Features/Shared/UI/ErrorLabel";
 
 const AccessDenied = () => {
-  const { logout, isLoggingOut } = useLogout();
+  const { isLoggingOut, logout } = useLogout();
+  const navigate = useNavigate();
+  const { isUserLoggedIn } = useAuthCtx();
+  const { user, isLoading, error } = useGetUserProfile(isUserLoggedIn);
+
+  if (isLoading) return <Loading />;
+  if (error)
+    return (
+      <div className="flex justify-center items-center h-screen flex-col bg-primary-gray-0">
+        <h1 className="text-xl font-bold text-primary-gray-700 mb-8">
+          خطا در گرفتن اطلاعات شما
+        </h1>
+
+        <button
+          onClick={() => logout()}
+          className="text-primary-gray-700 flex">
+          <MdLogin className="w-6 h-6 text-primary-blue-900" />
+          صفحه ورود.
+        </button>
+      </div>
+    );
   return (
     <div className="flex justify-center items-center h-screen flex-col bg-primary-gray-0">
       <h1 className="text-xl font-bold text-primary-gray-700 mb-8">
@@ -12,7 +37,7 @@ const AccessDenied = () => {
       <button
         disabled={isLoggingOut}
         onClick={() => {
-          logout();
+          navigate(`/${user!.role.toLowerCase()}/dashboard`);
         }}
         className="flex items-center gap-x-2">
         <MdLogin className="w-6 h-6 text-primary-blue-900" />
